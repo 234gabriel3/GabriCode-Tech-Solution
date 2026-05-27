739f1742e6206d655f64d2ffaed412beb41c90c4
RICHIE <oliigabriel7@gmail.com>
2026-05-27 12:05:57 -0800

    updated website design

---
+++ index.js
@@ -0,0 +1,19 @@
+// let formed = document.getElementById("formed");
+// let options = document.getElementById("options");
+// let optionss = document.getElementById("optionss");
+// let optionsss = document.getElementById("optionsss");
+
+
+// options.addEventListener("click", event => {
+//     formed.style.backgroundColor = "red";
+//     formed.textContent = "676";
+// });
+
+// optionss.addEventListener("click", event => {
+//     formed.style.backgroundColor = "red";
+//     formed.textContent = "676";
+// }); 
+// // optionsss.addEventListener("click", event => {
+// //     formed.style.backgroundColor = "red";
+// //     formed.textContent = "676";
+// // });
