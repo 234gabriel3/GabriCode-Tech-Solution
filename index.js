0d937571547948baff93b0b2a6d142dfa31315f2
RICHIE <oliigabriel7@gmail.com>
2026-06-05 08:08:52 -0800

    updated project file

--- index.js
+++ index.js
@@ -1,18 +1,18 @@
-// let formed = document.getElementById("formed");
-// let options = document.getElementById("options");
-// let optionss = document.getElementById("optionss");
-// let optionsss = document.getElementById("optionsss");
+let formed = document.querySelector("#formed");
+let options = document.querySelector("#options");
+let optionss = document.querySelector("#optionss");
+let optionsss = document.querySelector("#optionsss");
 
 
-// options.addEventListener("click", event => {
-//     formed.style.backgroundColor = "red";
-//     formed.textContent = "676";
-// });
+options.addEventListener("click", event => {
+    formed.style.backgroundColor = "red";
+    formed.textContent = "676";
+});
 
-// optionss.addEventListener("click", event => {
-//     formed.style.backgroundColor = "red";
-//     formed.textContent = "676";
-// }); 
+optionss.addEventListener("click", event => {
+    formed.style.backgroundColor = "red";
+    formed.textContent = "676";
+}); 
 // // optionsss.addEventListener("click", event => {
 // //     formed.style.backgroundColor = "red";
 // //     formed.textContent = "676";
